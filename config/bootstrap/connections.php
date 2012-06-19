b4295f3a507aa925a7ea1cb6858f071fe9a50364
Steve <Fastmoverr@gmail.com>
2012-06-19 09:42:37 -0500

    Removed db connection made from module
    A db connection now must be made by the app itself, named li3_users, or
    edit li3_users/models/user.php and change connection name.

--- config/bootstrap/connections.php
+++ config/bootstrap/connections.php
@@ -6,12 +6,12 @@
  * @license       http://www.opensource.org/licenses/MIT The MIT License
  */
 
-use lithium\data\Connections;
-
+//use lithium\data\Connections;
+/*
 Connections::add('li3_users', array(
 	'adapter' => 'MongoDb',
-	'host' => '192.168.1.250',
+	'host' => 'localhost',
 	'database' => 'li3_users'
 ));
-
+*/
 ?>
